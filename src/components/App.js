4eb5d16dddae3581cb987cfb4f4bd06bb470f82f
Florencia - Ubuntu 20.04 <ing.florencia.bergesio@gmail.com>
2021-08-31 14:04:12 -0300

    Call Login or NotebookCollection depending on user login info

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
+import { useContext } from 'react';
+import { UserContext } from '../context/UserContext';
 import './App.css';
+import Login from './login';
+import NotebookCollection from './notebook-collection';
 
-function App() {
+const App = () => {
+  const { user } = useContext( UserContext );
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Notekeeper</h1>
       </header>
+
+      { ( user.name === null ) 
+        ? <Login />
+        : <NotebookCollection />
+      }
+
     </div>
   );
-}
+};
 
 export default App;
