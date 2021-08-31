17e7281cbe947ae5f97a53fce5a652383d0a45f8
Florencia - Ubuntu 20.04 <ing.florencia.bergesio@gmail.com>
2021-08-31 17:18:58 -0300

    Added API call to get list of users and notebooks by user

--- src/components/notebook-collection/index.js
+++ src/components/notebook-collection/index.js
@@ -1,10 +1,48 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../../context/UserContext';
 import './index.css';
 import Logout from './../logout';
+import NotebooksContainer from '../notebooks-container';
 
 const NotebookCollection = () => {
-  const { user } = useContext( UserContext );
+  const { user, setUser } = useContext( UserContext );
+
+  const [ loading, setLoading ] = useState( false );
+  //const [ dataRetrieved, setDataRetrieved ] = useState( [] );
+  const [ notebooks, setNotebooks ] = useState( [] );
+
+  useEffect( () => {
+    setLoading( true );
+
+    const fetchData = async () => {
+      const urlAPI = "http://localhost:3000/users";
+      const response = await fetch(urlAPI);
+      const dataFromAPI = await response.json();
+      const userList = dataFromAPI.body;
+
+      if ( userList.length > 0 ) {
+        const existingUser = userList.find( element => {
+          return element.name === user.name;
+        } );
+
+        if ( existingUser ) {
+          setUser( { ...user, _id: existingUser._id } );
+          const fetchNotebooks = async ( existingUser ) => {
+            const urlAPI = "http://localhost:3000/notebooks?user=" + existingUser._id;
+            const response = await fetch(urlAPI);
+            const dataFromAPI = await response.json();
+            setNotebooks( dataFromAPI.body );
+          };
+
+          fetchNotebooks( existingUser );
+        }
+      }
+    };
+    
+    fetchData();
+
+    setLoading( false );
+  }, [ setLoading, setNotebooks ] );
 
   return (
     <div className="NotebookCollection">
@@ -13,6 +51,16 @@ const NotebookCollection = () => {
       </h2>
 
       <Logout />
+
+      { loading &&
+        <p className="loading">Loading...</p>
+      }
+
+      { ( notebooks.length > 0 ) 
+        ? <NotebooksContainer notebooks={ notebooks } />
+        : `You don't have any notebooks.`
+      }
+
     </div>
   );
 }
