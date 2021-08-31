a84d861b09f461f981b730257add297724a4360b
Florencia - Ubuntu 20.04 <ing.florencia.bergesio@gmail.com>
2021-08-31 14:02:07 -0300

    Added Login component

---
+++ src/components/login/index.js
@@ -0,0 +1,40 @@
+import { useRef, useContext } from 'react';
+import { UserContext } from '../../context/UserContext';
+import './index.css';
+import Button from './../button';
+
+const Login = () => {
+  const { setUser } = useContext( UserContext );
+
+  const refUserInput = useRef( null );
+
+  const handleFormSubmit = ( event ) => {
+    event.preventDefault();
+    const userInput = refUserInput.current.value;
+    setUser( { name: userInput } );
+  };
+
+  return (
+    <div className="Login">
+      <h2>
+        Login
+      </h2>
+
+      <form onSubmit={ handleFormSubmit }>
+        <label htmlFor="userInput">Name:</label>
+        <input 
+          type="text" 
+          id="userInput" 
+          placeholder="Your name"
+          ref={refUserInput}
+        />
+        <Button
+          type="Submit"
+          text="Login"
+        />
+      </form>
+    </div>
+  );
+}
+
+export default Login;
